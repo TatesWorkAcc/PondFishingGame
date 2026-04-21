10a8fd7178dd964b91245f0993d2a0633a1caa46
LilBakaTate <157347525+TatesWorkAcc@users.noreply.github.com>
2026-04-20 21:15:36 -0500

    Added new script for inventory drag

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+}
